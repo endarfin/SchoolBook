cf901248f3d86dbe49b808af8444acabcbed6d37
Andrey <zuborev.kh@gmail.com>
2020-04-12 22:03:34 +0300

    table schedule

--- resources/views/front/schedule/index.blade.php
+++ resources/views/front/schedule/index.blade.php
@@ -1,15 +1,50 @@
 @extends('template')
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <div class="float-rigth">New User</div>
-        </div>
-        <div class="card-body">
-        </div>
-        <div class="float-right">
-            <a class="btn btn-outline-info btn-sm"
-               href="{{ route('admin.users.index') }}">Back</a>
-            <button type="submit" class="btn btn-outline-info btn-sm">Save</button>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <div class="float-rigth">
+                        <div>Schedule</div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="card-title">
+                        <div>Subject:</div>
+                        <div>Group:</div>
+                        <div>Teacher:</div>
+                    </div>
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th rowspan="2">Students</th>
+                            <th>Data</th>
+                        </tr>
+                        <tr>
+                            <th>Оценки</th>
+                        </tr>
+
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <br>
+
+            <div class="float-right">
+                <a class="btn btn-outline-info btn-sm"
+                   href="{{ route('admin.users.index') }}">Back</a>
+                <button type="submit" class="btn btn-outline-info btn-sm">Save</button>
+            </div>
         </div>
     </div>
 @endsection
