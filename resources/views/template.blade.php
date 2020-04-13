489913e01ef2b6645defc2b42e3e3a19e70f07d1
Andrey <zuborev.kh@gmail.com>
2020-04-13 09:27:25 +0300

    template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -20,8 +20,8 @@
           <div class="float-left">
             <nav class="nav">
               <a class="nav-link" href="/" disabled>AcademyBook</a>
+              <a class="nav-link" href="/timetable">Расписание</a>
               <a class="nav-link" href="{{route('front.journals.index')}}">Электронный журнал</a>
-              <a class="nav-link" href="rank">Оценки</a>
               <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="       false">Наша Академия</a>
                     <div class="dropdown-menu alert-light">
