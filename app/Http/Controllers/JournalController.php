430b947219fdc03c2d2a4fae17bc7cd8ca0d6d9c
Andrey <zuborev.kh@gmail.com>
2020-04-16 15:57:31 +0300

    next

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Groups;
 use App\Models\Journal;
 use App\Models\Lessons;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Stmt\Foreach_;
 
 class JournalController extends Controller
 {
@@ -16,32 +19,43 @@ class JournalController extends Controller
      */
     public function index()
     {
-//        $journals = \DB::table('lessons')
-//            ->join('groups', 'lessons.group_id', '=', 'groups.id')
-//            ->join('users', 'users.group_id', '=', 'groups.id')
-//            ->join('subjects', 'lessons.subject_id', '=', 'subjects.id')
-//            ->leftJoin('journals', function ($join) {
-//                $join->on('lessons.id', '=', 'journals.lesson_id');
-//                $join->on('journals.student_id', '=', 'users.id');
-//            })
-//            ->select(\DB::raw('DATE_FORMAT(FROM_UNIXTIME(lessons.date_event), "%Y-%m-%d %h:%i:%s") as date'),
-//                'subjects.name as subject', 'groups.name as group', 'users.name as studentName', 'users.surname as studentSurname',
-//                'journals.exist', 'journals.mark')
-//            ->where('lessons.group_id', '=', '1')
-//            ->where('lessons.subject_id', '=', '2')
-//            ->get();
+        $students = \DB::table('users')
+            ->select('name', 'surname', 'login')
+            ->where('group_id','=','1')
+            ->get();
 
-        $lessons = Lessons::with('groups', 'subject', 'user', 'journal')->where([['group_id', 1],['subject_id',2]])->get();
+        $date = \DB::table('lessons')
+            ->select('lessons.date_event as date')
+            ->where([['group_id','=','1'],['subject_id','=','2']])
+            ->get();
 
+        $marks = \DB::table('lessons')
+            ->join('groups', 'lessons.group_id', '=', 'groups.id')
+            ->join('users', 'users.group_id', '=', 'groups.id')
+            ->join('subjects', 'lessons.subject_id', '=', 'subjects.id')
+            ->leftJoin('journals', function ($join) {
+                $join->on('lessons.id', '=', 'journals.lessons_id');
+                $join->on('journals.student_id', '=', 'users.id');
+            })
+            ->select('lessons.date_event as date', 'users.login as user', 'journals.mark as mark')
+            ->where([['lessons.group_id', '=', '1'],['lessons.subject_id', '=', '2']])
+            ->get();
 
-//        $journal = [];
-//        foreach ($journals as $row) {
-//            $journal[$row->subject][$row->date][$row->group][$row->studentSurname] = $row->mark;
-//        }
-       dd($lessons);
-//        dd($journal);
-//
-        return view('front.journals.index', compact( 'lessons'));
+        foreach ($marks as $mark) {
+            $schedule[$mark->user][$mark->date]= $mark->mark;
+        }
+        foreach ($date as $date) {
+            $day[] = $date->date;
+        }
+        $days = count($day);
+
+        foreach ($students as $student) {
+            $user[$student->login]= $student->surname.' '.$student->name;
+        }
+
+//        dd($abc);
+
+        return view('front.journals.index', compact( 'user', 'date', 'schedule', 'day', 'days'));
     }
 
     /**
--- resources/views/front/journals/index.blade.php
+++ resources/views/front/journals/index.blade.php
@@ -1,48 +1,67 @@
 @extends('template')
 @section('content')
 
-{{--            <div class="card">--}}
-{{--                <div class="card-header">--}}
-{{--                    <div class="float-rigth">--}}
-{{--                        <div>Journal</div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="card-body">--}}
-{{--                    <div class="card-title">--}}
-{{--                        <div>Subject: {{$lesson->subject->name}}</div>--}}
-{{--                        <div>Group: {{$lesson->groups->name}}</div>--}}
-{{--                    </div>--}}
-{{--                    <table class="table table-bordered table-hover">--}}
-{{--                        <thead>--}}
-{{--                        <tr>--}}
-{{--                            <th rowspan="2">Student's List</th>--}}
-{{--                            <th colspan="2">{{date("Y-m-d H:i", $lesson->date_event)}}</th>--}}
-{{--                        </tr>--}}
-{{--                        <tr>--}}
-{{--                            <th>Attend</th>--}}
-{{--                            <th>Marks</th>--}}
-{{--                        </tr>--}}
+    <div class="card">
+        <div class="card-header">
+            <div class="float-rigth">
+                <div>Journal</div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="card-title">
+                {{--                        <div>Subject: {{$lesson->subject->name}}</div>--}}
+                {{--                        <div>Group: {{$lesson->groups->name}}</div>--}}
+            </div>
+            <table class="table table-bordered table-hover">
 
-{{--                        </thead>--}}
-{{--                        <tbody>--}}
-{{--                        @foreach($journals as $journal)--}}
-{{--                            <tr>--}}
-{{--                                <th scope="row">{{$journal->studentSurname }} {{ $journal->studentName}}</th>--}}
-{{--                                <td>{{ $journal->exist}}</td>--}}
-{{--                                <td>{{ $journal->mark}}</td>--}}
+                {{--                <tr>--}}
+                {{--                    <th rowspan="2">Student's List</th>--}}
+                {{--                    @foreach($date as $date)--}}
+                {{--                        <th colspan="2" class="text-center">{{$date->date}}</th>--}}
+                {{--                    @endforeach--}}
+                {{--                </tr>--}}
 
-{{--                            </tr>--}}
-{{--                        @endforeach--}}
-{{--                        </tbody>--}}
-{{--                    </table>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <br>--}}
 
-{{--            <div class="float-right">--}}
-{{--                <a class="btn btn-outline-info btn-sm"--}}
-{{--                   href="{{ route('admin.users.index') }}">Back</a>--}}
-{{--                <button type="submit" class="btn btn-outline-info btn-sm">Save</button>--}}
-{{--            </div>--}}
+                {{--                @foreach($students as $student)--}}
+                {{--                    <tr>--}}
+                {{--                        <th>{{$student->surname }} {{ $student->name}}</th>--}}
+                {{--                    </tr>--}}
+                {{--                @endforeach--}}
+                @php
+                    echo '<thead>';
+                        echo '<tr>';
+                        echo '<th>Student</th>';
+                        for ($i = 0; $i < $days; $i++) {
+                            echo '<th>'.date("Y-m-d H:i:s", $day[$i]).'</th>';
+                        }
+                        echo '</tr>';
+                    echo '</thead>';
+                        echo '<tbody>';
+                        //foreach ($students as $student) {
+                         //   echo '<td>'.$student->surname.$student->name.'</td>';
+                       // }
+                            foreach ($schedule as $key => $value) {
+
+                                echo '<td>'.$user[$key].'</td>';
+                                for ($i = 0; $i < $days; $i++) {
+                                    if (empty($value[$day[$i]])) {
+                                        echo '<td> </td>';
+                                    } else echo '<td>'.$value[$day[$i]].'</td>';
+                                }
+                                echo "</tr>";
+                            }
+                            echo '</tbody>';
+                @endphp
+
+            </table>
+        </div>
+    </div>
+    <br>
+
+    <div class="float-right">
+        <a class="btn btn-outline-info btn-sm"
+           href="{{ route('admin.users.index') }}">Back</a>
+        <button type="submit" class="btn btn-outline-info btn-sm">Save</button>
+    </div>
 
 @endsection
